79dfa24acd600633af6c4088af92602065f92b1e
Ezequiel Ibarra Luna <cheque816@gmail.com>
2018-11-26 00:08:59 -0600

    Create punaje.php

---
+++ punaje.php
@@ -0,0 +1,2 @@
+<!-- Elaborar un algoritmo que permita ingresar el numero de partidos ganados, perdidos y empatados, por algun equipo en el torneo apertura, se debe de mostrar su puntaje total,
+teniendo en cuenta que por cada partido ganado obtendra 3 puntos, empatando 1 punto y perdido 0 puntos. -->
