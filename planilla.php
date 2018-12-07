d0bb4d5915d5f8368f76e399967f95350d62ee10
Ezequiel Ibarra Luna <cheque816@gmail.com>
2018-12-06 18:19:05 -0600

    Create planilla.php

---
+++ planilla.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Planilla de Empleado</title>
+</head>
+<body>
+	<h3>Planilla empleado</h3>
+	
+
+<script type="text/javascript">
+	var horas_laboradas = prompt("ingrese el numero de horas: ");
+	var costo_hora = prompt("El costo de las horas: ");
+	var total = 0;
+
+	total = parseInt(horas_laboradas) * parseInt(costo_hora);
+
+	document.write("lo gando al mes es de: " + total);
+
+</script>
+</body>
+</html>
+<?php 
+//Se requiere el algoritmo para elaborar la planilla de un empleado. Para ello se dispone de sus horas laboradas en el mes, asi como de la tarifa por hora.
+ ?>
